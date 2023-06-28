d0870a50ad9a1594a8ad6d57783e3a26dde053ea
root <michaelfavour418@gmail.com>
2023-06-27 20:03:07 -0700

    Task 0

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _printf - function that customizes the printf function
+ * @format: the format list
+ * Return: the printed chars
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int length;
+
+	va_start(args, format);
+	length = 0;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = (char)va_arg(args, int);
+
+						length += _putchar(c);
+						break;
+					}
+				case 's':
+					{
+						char *str = va_arg(args, char *);
+
+						length += _puts(str);
+						break;
+					}
+				case '%':
+					{
+						length += _putchar('%');
+						break;
+					}
+		default:
+					break;
+			}
+		} else
+		{
+			length += _putchar(*format);
+		} format++;
+	} va_end(args);
+	return (length);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _puts(const char *str);
+int _strlen(const char *str);
+int _printf(const char *format, ...);
+int _itoa(int value, char *str, int base);
+
+#endif /* MAIN_H */
