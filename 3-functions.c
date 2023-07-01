04bf3b2a4726070ee53283b1bab836067d98ce8d
root <michaelfavour418@gmail.com>
2023-06-30 18:15:01 -0700

    printf

--- 3-functions.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-/**
- * int _print_oct(unsigned int value)
- * int _print_bin(unsigned int value)
- * int _print_string(char *str, int precision)
- * int _print_pointer(void *value)
- */
